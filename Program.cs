6b4059324f69dcf85049813c5fe59741f44728d3
conno <conno@CONSTRAPCUM>
2024-11-06 13:55:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Formats.Asn1;
+
+namespace asignment_1._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareLength = 5.5;
+            double result = SquareArea(squareLength);
+            Console.WriteLine("Area of the square.");
+            Console.WriteLine(result);
+
+            double rectangleLength = 7.5;
+            double rectangleHeight = 8.5;
+            double result1 = (rectangleLength * rectangleHeight);
+            Console.WriteLine("Area of the rectangle.");
+            Console.WriteLine(result1);
+
+            double triangleBase = .5 * 5;
+            double triangleHeight = 6;
+            double result2 = (triangleBase * triangleHeight);
+            Console.WriteLine("Area of the triangle");
+            Console.WriteLine(result2);
+
+        }
+
+        static double SquareArea(double length)
+        {
+            double area = length * length;
+             
+            return area;
+
+            
+
+        }
+
+
+    }
+}
